6e07cdafd6fa47240d579f7d1a21d184f4a666b3
Am <am260700@gmail.com>
2020-03-18 17:43:23 -0700

    Updates to Assignment 3.

--- Assignment3.py
+++ Assignment3.py
@@ -11,53 +11,28 @@ def step1(img):
     final_img = np.zeros(img.shape)
     for i in range(img[0]):
         for j in range(img[1]):
-            # Decide that the pixel (i,j) is the border, increment i2,j2 <- (i, j-1)
             if img[i, j] == 1 and img[i, j-1] == 0:
+                # Decide that the pixel (i,j) is the border,
                 nbd += 1
-                img[i + 1, j +1] = img[i, j - 1]
-            # Decide that the pixel is the (i,j) is the border following starting point of a hole border, increment nbd
-            # (i2, j2) <- (i, j + 1) and LNBD <- fij in case fij > 1
+                # increment i2,j2 <- (i, j-1)
             elif img[i, j] >= 1 and img[i, j+1] == 0:
+                # Decide that the pixel is the (i,j) is the border following starting point of a hole border
                 nbd += 1
-                img[i+1, j+1] = img[i, j+1]
+                # (i2, j2) <- (i, j + 1)
                 if img[i, j] > 1:
                     lnbd = img[i, j]
             else:
                 lndb = step4(img, lnbd, i, j)
-            step2()
-            step3()
-
-
-def step2():
-    print("TODO")
-
-
-def step3():
-    print("TODO")
-
-
-def step3_1():
-    print("TODO")
-
-
-def step3_2():
-    print("TODO")
-
-
-def step3_3():
-    print("TODO")
-
-
-def step3_4():
-    print("TODO")
-
-
-def step3_5():
-    print("TODO")
+                continue
+            # Step 2
+            try:
+                if img[i - 1, j + 1] > 0 or img[i, j + 1] > 0 or img[i + 1, j + 1] > 0 or img[i - 1, j] > 0 or img[i + 1, j] > 0 or img[i - 1, j - 1] > 0 or img[i, j - 1] > 0 or img[i + 1, j - 1] > 0:
+            except IndexError:
+                pass
 
 
 def step4(img, lnbd, i, j):
-    if img[i][j]:
+    if img[i][j] != 1:
         lnbd = abs(img[i][j])
     return lnbd
 
